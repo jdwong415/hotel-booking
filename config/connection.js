2e426fa64197535e8bb4f2e020640c0806b1951d
Ken Wong <KenWong@calvisitor-10-105-147-167.calvisitor.1918.berkeley.edu>
2017-07-31 11:10:46 -0700

    created models

---
+++ config/connection.js
@@ -0,0 +1,24 @@
+var mysql = require("mysql");
+
+
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+		host: 'localhost',
+		user: 'root',
+		password: 'password',
+		database: 'hotel_db'
+	});
+};
+
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
