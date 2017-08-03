59f04875199d935c1e54992d4f60270668efb937
Jonathan Wong <jdwong2@yahoo.com>
2017-08-03 14:29:42 -0700

    Fix

--- controllers/table_controller.js
+++ controllers/table_controller.js
@@ -2,28 +2,25 @@ const express = require('express');
 const router = express.Router();
 const db = require("../models");
 
-
 //get all the res
 router.get('/tables', function(req, res) {
   db.Table.findAll({ 
   }).then(function(data) {
     var hbsObject = {tables: data};
-    console.log(hbsObject);
     res.render('tables', hbsObject);
   });
 });
 
 //adding a res to the list
 router.post('/tables', function(req, res) {
-	console.log(req.body);
-  	db.Table.create({
-  	name: req.body.name,
-  	phone: req.body.phone,
-  	email: req.body.email,
-    num_party: req.body.num_party,
-    res_time: req.body.res_time,
-   }).then(function(data) {
-   	  db.Table.update({
+	db.Table.create({
+	name: req.body.name,
+	phone: req.body.phone,
+	email: req.body.email,
+  num_party: req.body.num_party,
+  res_time: req.body.res_time,
+ }).then(function(data) {
+ 	  db.Table.update({
       GuestId: data.dataValues.id,
       available: false
     }, {
@@ -31,20 +28,20 @@ router.post('/tables', function(req, res) {
         id: req.body.id
       }
     }).then(function(data) {
-    res.redirect('/tables');
+      res.redirect('/tables');
+    });
   });
- });
 });
 
 //delete a res
 router.delete("/tables/:id", function(req, res) {
-    return db.Table.destroy({
-        where: {
-            id: req.params.id
-        }
-    }).then(function() {
-        res.redirect('/tables');
-    });
+  return db.Table.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(function() {
+    res.redirect('/tables');
+  });
 });
 
 module.exports = router;
